d6c6f2ab553b80f8e186c5f1c5b5057ae4e2fa8b
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-16 02:16:41 +0600

    First commit with basic setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,11 @@
+import {
+    createBrowserRouter,
+    
+} from "react-router-dom";
+  
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
