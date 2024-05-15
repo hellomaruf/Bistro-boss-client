a7483dd541d575114ee516866678ade561e76994
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-16 03:18:03 +0600

    Added Footer section

--- src/Shared/Nav.jsx
+++ src/Shared/Nav.jsx
@@ -1,48 +1,108 @@
+import { NavLink } from "react-router-dom";
 
 function Nav() {
+  const link = (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive
+            ? "text-yellow-200 pr-5 font-medium"
+            : "text-gray-800 pr-5 font-medium"
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/dashboard"
+        className={({ isActive }) =>
+          isActive
+            ? "text-yellow-200 pr-5 font-medium"
+            : "text-gray-800 pr-5 font-medium"
+        }
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        to="/ourMenu"
+        className={({ isActive }) =>
+          isActive
+            ? "text-yellow-200 pr-5 font-medium"
+            : "text-gray-800 pr-5 font-medium"
+        }
+      >
+        Our Menu
+      </NavLink>
+      <NavLink
+        to="/shopNow"
+        className={({ isActive }) =>
+          isActive
+            ? "text-yellow-200 pr-5 font-medium"
+            : "text-gray-800 pr-5 font-medium"
+        }
+      >
+        Shop Now
+      </NavLink>
+      <NavLink
+        to="/contactUs"
+        className={({ isActive }) =>
+          isActive
+            ? "text-yellow-200 pr-5 font-medium"
+            : "text-gray-800 pr-5 font-medium"
+        }
+      >
+        Contact Us
+      </NavLink>
+    </>
+  );
+
   return (
     <div>
-      <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      <div className=" bg-gray-900 bg-opacity-15 text-gray-900">
+        <div className="navbar max-w-7xl mx-auto">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                {link}
+              </ul>
+            </div>
+            <div className="">
+              <h2 className="text-3xl uppercase font-bold">Bistro Boss</h2>
+              <h4 className="tracking-[11px] text-lg">Restaurant</h4>
+            </div>
+          </div>
+
+          <div className="navbar-end">
+            <ul className="menu menu-horizontal px-1">{link}</ul>
+            <a className="btn">Button</a>
+          </div>
+        </div>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-    </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
