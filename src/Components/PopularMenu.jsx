465a95405c98176d1e3f141a775cf71f51a025cc
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-17 12:26:38 +0600

    show popular menu by using custome hooks

--- src/Components/PopularMenu.jsx
+++ src/Components/PopularMenu.jsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import SectionHeader from "./SectionHeader";
 import MenuCard from "../Shared/MenuCard";
+import useMenu from "../Hooks/useMenu";
 
 function PopularMenu() {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const menu = data.filter((menu) => menu.category === "popular");
-        setMenu(menu);
-      });
-  }, []);
-  console.log(menu);
+  const [menu] = useMenu();
+  const popularMenu = menu.filter((menu) => menu.category === "popular");
   return (
     <div className=" my-16">
       <SectionHeader shorts={"Check it out"} title={"FROM OUR MENU"} />
       <div className="max-w-7xl mx-auto grid grid-cols-2 gap-8">
-        {menu.map((item, index) => (
+        {popularMenu.map((item, index) => (
           <MenuCard key={index} menu={item} />
         ))}
       </div>
