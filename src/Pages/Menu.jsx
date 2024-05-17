f2d9a42798d6409b14eeabf462910567df9a12ab
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-17 16:49:03 +0600

    added Desserts cover

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover";
 import img1 from "../assets/menu/banner3.jpg";
+import des from "../assets/menu/dessert-bg.jpeg";
 import SectionHeader from "../Components/SectionHeader";
 import useMenu from "./../Hooks/useMenu";
 import MenuCard from "../Shared/MenuCard";
@@ -26,6 +27,14 @@ function Menu() {
           ))}
         </div>
       </div>
+      <Cover
+        height={"150px"}
+        maintitle={"Desserts"}
+        subtitle={
+          "Desserts, the sweet finales to meals, have been cherished and savored by cultures worldwide for centuries. These delectable treats come in a myriad of forms, flavors, and textures, offering something to delight every palate."
+        }
+        img={des}
+      />
     </div>
   );
 }
