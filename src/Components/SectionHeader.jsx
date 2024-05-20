35304969fc0bc9e5aa7854644758d69eb73a93ce
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-20 11:47:35 +0600

    Added my card table

--- src/Components/SectionHeader.jsx
+++ src/Components/SectionHeader.jsx
@@ -1,6 +1,6 @@
 function SectionHeader({ shorts, title }) {
   return (
-    <div className="text-center my-10">
+    <div className="text-center my-8">
       <p className="text-orange-300 text-xl">{shorts}</p>
       <h2 className="text-4xl font-semibold py-2 uppercase">{title}</h2>
     </div>
--- src/Pages/Dashboard/Cart.jsx
+++ src/Pages/Dashboard/Cart.jsx
@@ -1,10 +1,66 @@
+import SectionHeader from "../../Components/SectionHeader";
+import useCarts from "../../Hooks/useCarts";
+import { RiDeleteBinLine } from "react-icons/ri";
 
 function Cart() {
+  const [cart] = useCarts();
+  console.log(cart);
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   return (
     <div>
-      My cart
+      <SectionHeader shorts={"---My Cart---"} title={"WANNA ADD MORE?"} />
+      <div className="mx-14 bg-orange-50 p-4">
+        <div className="flex justify-between items-center pb-4">
+          <h2 className="text-2xl font-medium">Total Order : {cart.length}</h2>
+          <h2 className="text-2xl font-medium">Total Price : {totalPrice}</h2>
+          <button className="btn bg-orange-400 hover:bg-orange-500 text-white">
+            Pay
+          </button>
+        </div>
+        {/* table */}
+        <div className="">
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead className="bg-orange-400 rounded-xl">
+                <tr>
+                  <th className="text-base text-white">Item Image</th>
+                  <th className="text-base text-white">Item Name</th>
+                  <th className="text-base text-white">Price</th>
+                  <th className="text-base text-white">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((item, index) => (
+                  <tr key={index}>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img
+                              src={item?.image}
+                              alt="Avatar Tailwind CSS Component"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>{item?.name}</td>
+                    <td>{item?.price}</td>
+                    <th>
+                      <button className="btn bg-red-500 text-white hover:bg-red-600">
+                        <RiDeleteBinLine />
+                      </button>
+                    </th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
